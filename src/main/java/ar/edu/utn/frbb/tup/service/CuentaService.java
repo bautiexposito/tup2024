362af7deea8cc313359b6e3b181b5b51b944e322
bautiexposito <114201372+bautiexposito@users.noreply.github.com>
2024-06-14 20:23:32 -0300

    Update CuentaService.java

--- src/main/java/ar/edu/utn/frbb/tup/service/CuentaService.java
+++ src/main/java/ar/edu/utn/frbb/tup/service/CuentaService.java
@@ -1,7 +1,9 @@
 package ar.edu.utn.frbb.tup.service;
 
 import ar.edu.utn.frbb.tup.model.Cuenta;
+import ar.edu.utn.frbb.tup.model.TipoCuenta;
 import ar.edu.utn.frbb.tup.model.exception.CuentaAlreadyExistsException;
+import ar.edu.utn.frbb.tup.model.exception.CuentaNoSoportadaException;
 import ar.edu.utn.frbb.tup.model.exception.TipoCuentaAlreadyExistsException;
 import ar.edu.utn.frbb.tup.persistence.CuentaDao;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,22 +18,22 @@ public class CuentaService {
 
     @Autowired
     ClienteService clienteService;
-
-    //Generar casos de test para darDeAltaCuenta
-    //    1 - cuenta existente
-    //    2 - cuenta no soportada
-    //    3 - cliente ya tiene cuenta de ese tipo
-    //    4 - cuenta creada exitosamente
-    public void darDeAltaCuenta(Cuenta cuenta, long dniTitular) throws CuentaAlreadyExistsException, TipoCuentaAlreadyExistsException {
+    
+    public void darDeAltaCuenta(Cuenta cuenta, long dniTitular) throws CuentaAlreadyExistsException, TipoCuentaAlreadyExistsException, CuentaNoSoportadaException {
         if(cuentaDao.find(cuenta.getNumeroCuenta()) != null) {
             throw new CuentaAlreadyExistsException("La cuenta " + cuenta.getNumeroCuenta() + " ya existe.");
         }
 
-        //Chequear cuentas soportadas por el banco CA$ CC$ CAU$S
-        // if (!tipoCuentaEstaSoportada(cuenta)) {...}
+        if (!tipoCuentaEstaSoportada(cuenta)) {
+            throw new CuentaNoSoportadaException("El tipo de cuenta no está soportado.");
+        }
 
         clienteService.agregarCuenta(cuenta, dniTitular);
         cuentaDao.save(cuenta);
+    }
+
+    private boolean tipoCuentaEstaSoportada(Cuenta cuenta){
+        return cuenta.getTipoCuenta() == TipoCuenta.CUENTA_CORRIENTE || cuenta.getTipoCuenta() == TipoCuenta.CAJA_AHORRO;
     }
 
     public Cuenta find(long id) {
