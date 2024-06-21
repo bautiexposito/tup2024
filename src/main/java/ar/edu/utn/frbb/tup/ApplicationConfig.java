59fd80e024b9703379f71548ac23f7fc776988f4
bautiexposito <114201372+bautiexposito@users.noreply.github.com>
2024-06-21 15:47:27 -0300

    Update ApplicationConfig.java

--- src/main/java/ar/edu/utn/frbb/tup/ApplicationConfig.java
+++ src/main/java/ar/edu/utn/frbb/tup/ApplicationConfig.java
@@ -1,6 +1,6 @@
 package ar.edu.utn.frbb.tup;
 
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
